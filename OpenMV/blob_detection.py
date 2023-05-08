73906f457b77b49b449242148d5a89158fecb146
4simb <63647269+4simb@users.noreply.github.com>
2023-05-08 17:56:19 +0300

    changed thresholds

--- OpenMV/blob_detection.py
+++ OpenMV/blob_detection.py
@@ -32,14 +32,14 @@ def set_led_off():
     green_led.off()
     blue_led.off()
 
-threshold_black = (0, 11, -35, 127, -128, 127)#(0, 14, -35, 127, -128, 127)#(0, 18, -128, 127, 127, -128)
-threshold_yellow = (19, 100, -128, 24, 39, 127)#(60, 100, -128, 24, 42, 127)#(100, 0, -128, 24, 32, 127)#(0, 100, -48, 127, -128, 45)#
-threshold_green = (0, 21, -128, -8, -128, 34)#(0, 100, -128, -15, -128, 35)#(0, 31, -128, -15, -16, 22)#
-threshold_red = (0, 60, 27, 127, 24, 127)#(0, 60, 24, 127, 5, 127)#(0, 21, 24, 127, 5, 127)
+threshold_black = (0, 19, -18, 127, -128, 12)#(0, 11, -35, 127, -128, 127)
+threshold_yellow = (19, 100, -128, 24, 39, 127)
+threshold_green = (0, 21, -128, -8, -128, 34)
+threshold_red = (0, 60, 27, 127, 0, 127)#(0, 60, 27, 127, 24, 127)
 messageOld = " "
 
 sensor.reset()
-sensor.set_contrast(1)              # Reset and initialize the sensor.
+sensor.set_contrast(1)
 sensor.set_gainceiling(16)
 sensor.set_pixformat(sensor.RGB565) #GRAYSCALE) # Set pixel format to RGB565 (or GRAYSCALE)
 sensor.set_framesize(sensor.QVGA)   # Set frame size to QVGA (160x120)
@@ -48,9 +48,8 @@ clock = time.clock()                # Create a clock object to track the FPS.
 uart = UART(3, 115200)
 
 while(True):
-    #set_led_white()
-    clock.tick()                    # Update the FPS clock.
-    img = sensor.snapshot()         # Take a picture and return the image.
+    clock.tick()
+    img = sensor.snapshot()
     img.replace(img, vflip = True, hmirror = True)
 
     message = " "
@@ -58,7 +57,6 @@ while(True):
     colorLetter = " "
     foundColor = False
     trueBlob = False
-    prevBlob = False
 
     #COLOR START
 
@@ -90,36 +88,28 @@ while(True):
             colorLetter = "Y"
 
     if foundColor:
+        #set_led_white()
         if trueBlob.w() / trueBlob.h() > 2:
             continue
         img.draw_rectangle(trueBlob.x(), trueBlob.y(), trueBlob.w(), trueBlob.h(), color = (255, 0, 255))
         print(colorLetter, trueBlob.x(), trueBlob.y())
         uart.write(colorLetter)
         message = colorLetter
+        #set_led_off()
 
     #COLOR EnD
 
-    else:
+    else: #no color found
         for letter in img.find_blobs([threshold_black], pixel_threshold = 2000, area_threshold = 1000, merge = True, margin = 100):#area_threshold = 2000):
             ratio = letter.w()/letter.h()
             if ratio < 0.4 or ratio > 2.5:
                 continue
-            #if lBlob.w()/lBlob.h() > 0.4 and yb.w()/yb.h() < 2.5:
 
             # true blob sizes
             bx = max(letter.x(), 0)
             by = max(letter.y(), 0)
             bw = min(img.width() - letter.x(), letter.w())
             bh = min(img.height() - letter.y(), letter.h())
-
-            #count = 0
-            #                 bw
-            #    (bx, by) --------- (bx + bw, by)
-            #        |                     |
-            #        |                     |  bh
-            #        |                     |
-            #        |                     |
-            #  (bx, by + bh) --- (bx + bw, by + bh)
 
             upRoi = bx, by, bw, int(bh/4)
             midRoi = bx, by + int(bh * 1 / 4), bw, int(bh/2)
@@ -129,73 +119,70 @@ while(True):
             vertRoi = bx + int(bw / 3), by, int(bw/3), bh
             rightRoi = bx + int(bw * 2 / 3), by, bx + bw, bh
 
-            img.draw_rectangle(bx + int(bw / 3), by, int(bw / 3), bh, color = (50, 50, 50))
-            img.draw_rectangle(bx, by + int(bh / 3), bw, int(bh / 3), color = (50, 50, 50))
+            #count blobs in 6 sectors
+            upBlob = 0; midBlob = 0; downBlob = 0; leftBlob = 0; vertBlob = 0; rightBob = 0
+            upCount = 0; midCount = 0; downCount = 0; leftCount = 0; vertCount = 0; rightCount = 0
 
-            # roi = region of interest
+            blobAreaThreshold = 100;
 
-            upCount = 0
-            for upBlob in img.find_blobs([threshold_black], roi = upRoi, area_threshold = 0, pixel_threshold = 0, x_stride = 1, merge = True, margin = 10):
+            for upBlob in img.find_blobs([threshold_black], roi = upRoi, area_threshold = blobAreaThreshold, pixel_threshold = 200, x_stride = 1, merge = True, margin = 10):
                 upCount += 1;
                 img.draw_rectangle(upBlob.x(), upBlob.y(), upBlob.w(), upBlob.h(), color = (255, 255, 255))
 
-            midCount = 0
-            for midBlob in img.find_blobs([threshold_black], roi = midRoi, area_threshold = 0, pixel_threshold = 0, x_stride = 1, merge = True, margin = 10):
+            for midBlob in img.find_blobs([threshold_black], roi = midRoi, area_threshold = blobAreaThreshold, pixel_threshold = 200, x_stride = 1, merge = True, margin = 10):
                 midCount += 1;
                 img.draw_rectangle(midBlob.x(), midBlob.y(), midBlob.w(), midBlob.h(), color = (255, 255, 255))
 
-            downCount = 0
-            for downBlob in img.find_blobs([threshold_black], roi = downRoi, area_threshold = 0, pixel_threshold = 0, x_stride = 1, merge = True, margin = 10):
+            for downBlob in img.find_blobs([threshold_black], roi = downRoi, area_threshold = blobAreaThreshold, pixel_threshold = 200, x_stride = 1, merge = True, margin = 10):
                 downCount += 1
                 img.draw_rectangle(downBlob.x(), downBlob.y(), downBlob.w(), downBlob.h(), color = (255, 255, 255))
 
-            leftCount = 0
-            for leftBlob in img.find_blobs([threshold_black], roi = leftRoi, area_threshold = 0, pixel_threshold = 0, x_stride = 1, merge = True, margin = 10):
+            for leftBlob in img.find_blobs([threshold_black], roi = leftRoi, area_threshold = blobAreaThreshold, pixel_threshold = 200, x_stride = 1, merge = True, margin = 10):
                 leftCount += 1
                 img.draw_rectangle(leftBlob.x(), leftBlob.y(), leftBlob.w(), leftBlob.h(), color = (255, 255, 255))
 
-            vertCount = 0
-            for vertBlob in img.find_blobs([threshold_black], roi = vertRoi, area_threshold = 0, pixel_threshold = 0, x_stride = 1, merge = True, margin = 10):
+            for vertBlob in img.find_blobs([threshold_black], roi = vertRoi, area_threshold = blobAreaThreshold, pixel_threshold = 200, x_stride = 1, merge = True, margin = 10):
                 vertCount += 1
                 img.draw_rectangle(vertBlob.x(), vertBlob.y(), vertBlob.w(), vertBlob.h(), color = (255, 255, 255))
 
-            rightCount = 0
-            for rightBlob in img.find_blobs([threshold_black], roi = rightRoi, area_threshold = 0, pixel_threshold = 0, x_stride = 1, merge = True, margin = 10):
+            for rightBlob in img.find_blobs([threshold_black], roi = rightRoi, area_threshold = blobAreaThreshold, pixel_threshold = 200, x_stride = 1, merge = True, margin = 10):
                 rightCount += 1
                 img.draw_rectangle(rightBlob.x(), rightBlob.y(), rightBlob.w(), rightBlob.h(), color = (255, 255, 255))
 
+            img.draw_rectangle(bx + int(bw / 3), by, int(bw / 3), bh, color = (0, 200, 200))
+            img.draw_rectangle(bx, by + int(bh / 3), bw, int(bh / 3), color = (0, 200, 200))
+
             #determine letter
-            #if midCount == 1 and upCount == 1 and vertCount >= 2 and rightCount == 2:
-            #if vertCount >= 2 and upCount == 1 and midCount == 1:
-            #u: leftCount == 1 and vertCount == 1 and rightCount == 1 and upCount == 2 and downCount == 1 and midCount == 2
-
-
             sCondition = (vertCount >= 2 and rightCount >= 2 and leftCount >= 2) or (upCount == 1 and midCount == 2 and downCount == 1)
             hCondition = leftCount == 1 and vertCount == 1 and rightCount == 1 and upCount == 2 and midCount == 1 and downCount == 2
             uCondition = leftCount == 1 and vertCount == 1 and downCount == 1 and midCount == 2 and upCount == 2
 
+            if (leftCount, vertCount, rightCount, upCount, midCount, downCount) == (1, 1, 1, 1, 1, 1):
+                if midBlob.w() / downBlob.w() > 2:
+                    hCondition = True
+                    uCondition = False
+                elif downBlob.w() / midBlob.w() > 2:
+                    uCondition = True
+                    hCondition = False
+
+            recLetter = 0;
+
             if sCondition:
-                print("S")
-                message = "S"
-                uart.write("S")
-                #state = True
-                foundLetter = True
-                img.draw_rectangle(bx, by, bw, bh, color = (0, 0, 255))
-            #elif midCount == 1 and upCount == 2 and vertCount == 1:
+                recLetter = "S"
             elif hCondition:
-                print("H")
-                message = "H"
-                uart.write("H")
-                foundLetter = True
-                img.draw_rectangle(bx, by, bw, bh, color = (0, 0, 255))
-            #elif midCount == 2 and upCount == 2 and vertCount == 1:
+                recLetter = "H"
             elif uCondition:
-                print("U")
-                message = "U"
-                uart.write("U")
-                foundLetter = True
+                recLetter = "U"
+
+            if recLetter != 0:
+                #set_led_white()
+                foundLetter = True;
+                print(recLetter)
+                message = recLetter
+                uart.write(recLetter)
                 img.draw_rectangle(bx, by, bw, bh, color = (0, 0, 255))
-                #red_led.on()
+
+                #set_led_off()
 
     if messageOld != " " and message ==  " ":
         uart.write("0")
